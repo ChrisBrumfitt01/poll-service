f2bdec3b5731ed525b7617a711b973f1644e613a
chris <chrisbrumfitt@hotmail.co.uk>
2025-04-28 15:06:58 +0100

    Initial commit

---
+++ src/main/java/com/dizplai/pollservice/PollServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dizplai.pollservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollServiceApplication.class, args);
+	}
+
+}
