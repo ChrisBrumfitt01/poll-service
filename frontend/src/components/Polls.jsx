d28578e7b9086110aff6cac701b2836fa8433cdb
chris <chrisbrumfitt@hotmail.co.uk>
2025-05-04 13:42:19 +0100

    Add error and loading spinner to the polls component

--- frontend/src/components/Polls.jsx
+++ frontend/src/components/Polls.jsx
@@ -1,19 +1,29 @@
 import { useEffect, useState } from "react"
 import { getActivePolls } from '../service/httpClient.js'
 import Poll from './Poll.jsx'
+import Error from './Error.jsx'
 import './Polls.css';
+import LoadingSpinner from "./LoadingSpinner.jsx";
+
 
 
 export default function Polls() {
     const [polls, setPolls] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
 
     useEffect(() => {
         const fetchActivePolls = async () => {
             try {
+                setLoading(true);
                 const activePolls = await getActivePolls();
                 setPolls(activePolls);
+                setError(false);
             } catch (error) {
                 console.log("Failed to get active polls");
+                setError(true)
+            } finally {
+                setLoading(false)
             }
         }
         fetchActivePolls();
@@ -23,11 +33,22 @@ export default function Polls() {
     return (
         <section className="polls-container">
             <h2>Our currently active polls:</h2>
-            {polls.map((poll) => (
+
+            {error && <Error>An error occurred when trying to retrieve polls. Please 
+                try again later</Error>}
+
+            {loading && <LoadingSpinner/>}
+
+            {polls.length > 0 && polls.map((poll) => (
                 <div className="poll-container">
                     <Poll key={poll.id} data={poll} />
                 </div>
             ))}
+
+            {!loading && !error && polls.length == 0 && (
+                <p>There are no active polls available</p>
+            )}
+
         </section>
     );
 
