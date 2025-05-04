0351f4a23d22d5c0f70a48a750ac1952d51716fa
chris <chrisbrumfitt@hotmail.co.uk>
2025-05-04 21:41:08 +0100

    Fixed naming

--- backend/src/test/java/com/dizplai/pollservice/service/aggregator/VoteAggregatorTest.java
+++ backend/src/test/java/com/dizplai/pollservice/service/aggregator/VoteAggregatorTest.java
@@ -13,7 +13,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-class VoteAggregatorIntegrationTest {
+class VoteAggregatorTest {
 
     private VoteAggregator voteAggregator;
 
